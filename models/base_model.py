7c9ff76de3113fbad3883faa79a1e901754def53
travis1999 <trevoroguna@gmail.com>
2021-12-21 00:54:41 +0300

    base_model: refactor and default values
    if kwargs is passed and does not have created_at or modified_at
    they will be given default values

--- models/base_model.py
+++ models/base_model.py
@@ -15,10 +15,10 @@ class BaseModel:
             self.updated_at = datetime.now()
             storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.now() if 'created_at' not in kwargs\
+                else datetime.fromisoformat(kwargs['created_at'])
+            kwargs['updated_at'] = datetime.now() if 'updated_at' not in kwargs\
+                else datetime.fromisoformat(kwargs['updated_at'])
             del kwargs['__class__']
             self.__dict__.update(kwargs)
 
