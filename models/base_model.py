7ba7a7edd3ef346797fe11eea1c468cc47f60f9a
travis1999 <trevoroguna@gmail.com>
2021-12-21 09:45:08 +0300

    BaseModel.__init__: changes to argument parsing
    use of inspect.Signature to parse arguments

--- models/base_model.py
+++ models/base_model.py
@@ -2,29 +2,41 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from inspect import Signature, Parameter
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            kwargs['created_at'] = datetime.now() if 'created_at' not in kwargs\
-                else datetime.fromisoformat(kwargs['created_at'])
-            kwargs['updated_at'] = datetime.now() if 'updated_at' not in kwargs\
-                else datetime.fromisoformat(kwargs['updated_at'])
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+        base_signature = Signature(
+            [Parameter('id', Parameter.POSITIONAL_OR_KEYWORD,
+                       default=str(uuid.uuid4())),
+             Parameter('created_at', Parameter.POSITIONAL_OR_KEYWORD,
+                       default=datetime.now()),
+             Parameter('updated_at', Parameter.POSITIONAL_OR_KEYWORD,
+                       default=datetime.now())])
+
+        filter_dict = \
+            {param.name for param in base_signature.parameters.values()}
+        new_kwargs = {k: v for k, v in kwargs.items() if k in filter_dict}
+        params = base_signature.bind(*args, **new_kwargs)
+        params.apply_defaults()
+
+        self.__dict__.update(params.arguments)
+        self.to_datetime("created_at")
+        self.to_datetime("updated_at")
+
+    def to_datetime(self, attr):
+        """To datetime if attr is isostring"""
+        if isinstance(self.__dict__[attr], str):
+            self.__setattr__(attr,
+                             datetime.fromisoformat(self.__dict__[attr]))
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        cls = self.__class__.__name__
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
