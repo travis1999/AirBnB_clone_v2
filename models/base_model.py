b328a019adc7926f196ba92d48521ff08dae5674
travis1999 <trevoroguna@gmail.com>
2021-12-22 10:43:54 +0300

    storage: new storage type DBstorage
    models modified to support db storage

--- models/base_model.py
+++ models/base_model.py
@@ -3,30 +3,36 @@
 import uuid
 from datetime import datetime
 from inspect import Signature, Parameter
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column
+from sqlalchemy import DateTime
+from sqlalchemy import String
+import models
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        base_signature = Signature(
-            [Parameter('id', Parameter.POSITIONAL_OR_KEYWORD,
-                       default=str(uuid.uuid4())),
-             Parameter('created_at', Parameter.POSITIONAL_OR_KEYWORD,
-                       default=datetime.now()),
-             Parameter('updated_at', Parameter.POSITIONAL_OR_KEYWORD,
-                       default=datetime.now())])
+        """Initialize a new BaseModel.
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        self.id = str(uuid4())
+        self.created_at = self.updated_at = datetime.utcnow()
 
-        filter_dict = \
-            {param.name for param in base_signature.parameters.values()}
-        new_kwargs = {k: v for k, v in kwargs.items() if k in filter_dict}
-        params = base_signature.bind(*args, **new_kwargs)
-        params.apply_defaults()
-
-        self.__dict__.update(params.arguments)
-        self.to_datetime("created_at")
-        self.to_datetime("updated_at")
+        if kwargs:
+            for key, value in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    value = datetime.fromisoformat(value)
+                elif key != "__class__":
+                    setattr(self, key, value)
 
     def to_datetime(self, attr):
         """To datetime if attr is isostring"""
@@ -41,9 +47,9 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
-        self.updated_at = datetime.now()
-        storage.save()
+        self.updated_at = datetime.utcnow()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -51,4 +57,14 @@ class BaseModel:
         dictionary['__class__'] = self.__class__.__name__
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        dictionary.pop("_sa_instance_state", None)
         return dictionary
+
+    def delete(self):
+        """Delete the current instance from storage."""
+        models.storage.delete(self)
+
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        d.pop("_sa_instance_state", None)
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
